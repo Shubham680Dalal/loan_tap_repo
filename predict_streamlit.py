f5610ebf07a506b33d5636c8758194270c6bdd6e
Shubham680Dalal <dalalshubham14@gmail.com>
2024-09-18 19:37:18 +0530

    after_training

---
+++ predict_streamlit.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("This is a header with a divider")
+st.markdown("---") 
+st.header("These headers have rotating dividers")
+st.markdown("---") 
+st.header("One")
+st.markdown("---") 
+st.header("Two")
+st.markdown("---") 
+st.header("Three")
+st.markdown("---") 
+st.header("Four")
